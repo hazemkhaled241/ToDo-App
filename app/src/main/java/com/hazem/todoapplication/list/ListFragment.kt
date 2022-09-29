e922f540b78bc1cc42dce9cbce34f81dd00d6b20
hazem khaled <hazemk778@gmail.com>
2022-09-29 16:32:44 +0200

    list fragment menu completely done

--- app/src/main/java/com/hazem/todoapplication/list/ListFragment.kt
+++ app/src/main/java/com/hazem/todoapplication/list/ListFragment.kt
@@ -1,16 +1,16 @@
 package com.hazem.todoapplication.list
 
 import android.os.Bundle
+import android.view.*
+import android.widget.Toast
 
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.hazem.todoapplication.R
 
-class ListFragment : Fragment() {
+class ListFragment : Fragment(),MenuProvider {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,7 +23,17 @@ class ListFragment : Fragment() {
           floatingActionButton.setOnClickListener {
           findNavController().navigate(R.id.action_listFragment_to_addFragment)
       }
+     activity?.addMenuProvider(this)
     return view
+    }
+
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.list_fragment_menu,menu)
+    }
+
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        return true
+
     }
 
 }
