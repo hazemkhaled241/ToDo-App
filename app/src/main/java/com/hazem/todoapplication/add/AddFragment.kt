0177aeecd137f7a0cc90146c85dee6e9a556e1b7
hazem khaled <hazemk778@gmail.com>
2022-09-29 18:06:46 +0200

    add and update fragments menu are completely done

--- app/src/main/java/com/hazem/todoapplication/add/AddFragment.kt
+++ app/src/main/java/com/hazem/todoapplication/add/AddFragment.kt
@@ -1,22 +1,38 @@
 package com.hazem.todoapplication.add
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.Toolbar
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import com.hazem.todoapplication.R
 
 
-class AddFragment : Fragment() {
+class AddFragment : Fragment(),MenuProvider {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        activity?.addMenuProvider(this)
         return inflater.inflate(R.layout.fragment_add, container, false)
     }
+
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+       menuInflater.inflate(R.menu.add_fragment_menu,menu)
+        val searchItem = menu.findItem(R.id.menu_search)
+        val sortByItem = menu.findItem(R.id.menu_sortBy)
+        val deleteAllItem = menu.findItem(R.id.menu_deleteAll)
+        searchItem.isVisible = false
+        sortByItem.isVisible=false
+        deleteAllItem.isVisible=false
+    }
+
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        return true
+    }
+
 
 
 }
--- app/src/main/java/com/hazem/todoapplication/update/UpdateFragment.kt
+++ app/src/main/java/com/hazem/todoapplication/update/UpdateFragment.kt
@@ -1,20 +1,34 @@
 package com.hazem.todoapplication.update
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import com.hazem.todoapplication.R
 
-class UpdateFragment : Fragment() {
+class UpdateFragment : Fragment(),MenuProvider {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        activity?.addMenuProvider(this)
         return inflater.inflate(R.layout.fragment_update, container, false)
+    }
+
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+       menuInflater.inflate(R.menu.update_fragment_menu,menu)
+        val searchItem = menu.findItem(R.id.menu_search)
+        val sortByItem = menu.findItem(R.id.menu_sortBy)
+        val deleteAllItem = menu.findItem(R.id.menu_deleteAll)
+        searchItem.isVisible = false
+        sortByItem.isVisible=false
+        deleteAllItem.isVisible=false
+    }
+
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        return true
     }
 
 }
