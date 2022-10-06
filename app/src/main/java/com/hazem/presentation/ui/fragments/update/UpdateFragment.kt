9cfc9fd3bfb166e913668aa96c374ff4eb00e802
hazem khaled <hazemk778@gmail.com>
2022-10-06 23:31:54 +0200

    new feature

--- app/src/main/java/com/hazem/presentation/ui/fragments/update/UpdateFragment.kt
+++ app/src/main/java/com/hazem/presentation/ui/fragments/update/UpdateFragment.kt
@@ -25,7 +25,6 @@ private val args by navArgs<UpdateFragmentArgs>()
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View?
-
     {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_update, container, false)
