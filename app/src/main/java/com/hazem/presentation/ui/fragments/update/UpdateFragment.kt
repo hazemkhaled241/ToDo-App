a557cb23b6fe1da9d317e192a1510a16f406dcea
hazem khaled <hazemk778@gmail.com>
2022-10-06 21:27:37 +0200

    changes3

--- app/src/main/java/com/hazem/presentation/ui/fragments/update/UpdateFragment.kt
+++ app/src/main/java/com/hazem/presentation/ui/fragments/update/UpdateFragment.kt
@@ -29,6 +29,9 @@ private val args by navArgs<UpdateFragmentArgs>()
         return inflater.inflate(R.layout.fragment_update, container, false)
     }
 
+
+
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         activity?.addMenuProvider(this,viewLifecycleOwner,Lifecycle.State.RESUMED)
