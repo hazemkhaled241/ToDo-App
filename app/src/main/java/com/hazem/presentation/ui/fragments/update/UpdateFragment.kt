2546237acb15d9d569388804a217b6db0456d585
hazem khaled <hazemk778@gmail.com>
2022-10-06 22:58:53 +0200

    new feature2

--- app/src/main/java/com/hazem/presentation/ui/fragments/update/UpdateFragment.kt
+++ app/src/main/java/com/hazem/presentation/ui/fragments/update/UpdateFragment.kt
@@ -24,7 +24,8 @@ private val args by navArgs<UpdateFragmentArgs>()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View?
+    {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_update, container, false)
     }
